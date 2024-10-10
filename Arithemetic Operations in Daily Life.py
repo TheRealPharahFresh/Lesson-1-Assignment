4817f1227fe298d929dab8a09695824a97c6d2f2
TheRealPharahFresh <therealpharaohfresh@gmail.com>
2024-10-10 09:59:48 -0400

    second commit

--- Arithemetic Operations in Daily Life.py
+++ Arithemetic Operations in Daily Life.py
@@ -1,7 +1,7 @@
 #Task 1: /// Grocery Store Math
 #  Calculate the total cost of three items you'd commonly find in a grocery store, 
 # given their individual prices. 
-# For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic! ///
+# For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic! /// push -u
 
 bread = 4.00
 peanut_butter = 2.00
